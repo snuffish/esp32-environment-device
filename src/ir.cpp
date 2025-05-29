094146e99cdd6d19d2655dc6f4fd5acb7647602c
Snuffish <snuffish90@gmail.com>
2025-05-29 20:51:32 +0200

    wip

--- src/ir.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by snuffish on 2025-05-25.
-//
-
-#include "ir.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,27 @@
-#include <Arduino.h>
-#include <IRremoteESP8266.h>
-#include <IRsend.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <SPI.h>
 
-// Codes are in Global Cache format less the emitter ID and request ID.
-// These codes can be found in GC's Control Tower database.
+#define ST7735_DRIVER
+#define TFT_WIDTH 128
+#define TFT_HEIGHT 160
+#define ST7735_INITB
+#define TFT_CS 5 // Chip select control pin
+#define TFT_DC 2 // Data Command control pin
+#define TFT_RST 4 // Reset pin (optional)
+#define SPI_FREQUENCY 27000000
 
-uint16_t Samsung_power_toggle[71] = {
-    38000, 1, 1, 170, 170, 20, 63, 20, 63, 20, 63, 20, 20, 20, 20,
-    20, 20, 20, 20, 20, 20, 20, 63, 20, 63, 20, 63, 20, 20, 20,
-    20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 63, 20, 20, 20, 20,
-    20, 20, 20, 20, 20, 20, 20, 20, 20, 63, 20, 20, 20, 63, 20,
-    63, 20, 63, 20, 63, 20, 63, 20, 63, 20, 1798
-};
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
-#define IR_LED 4  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
-
-IRsend irsend(IR_LED); // Set the GPIO to be used to sending the message.
-
-void setup()
-{
-    irsend.begin();
-    Serial.begin(9600);
+void setup() {
+    tft.initR(INITR_GREENTAB); // Initialize the display with the correct tab color
+    tft.fillScreen(ST7735_BLACK); // Clear the screen
+    tft.setTextColor(ST7735_WHITE); // Set text color to white
+    tft.setTextSize(2); // Set text size
+    tft.setCursor(10, 10); // Set cursor position
+    tft.print("Hello, World!"); // Print text
 }
 
-void loop()
-{
-    // Send NEC code with UserCode 00EF and DataCode 04FB
-    uint16_t userCode = 0x00EF;  // UserCode in hexadecimal
-    uint16_t dataCode = 0x04FB;  // DataCode in hexadecimal
-    uint32_t combinedCode = (userCode << 16) | dataCode;  // Combine into full 32-bit code
-
-    Serial.println("Sending IR signal");
-    irsend.sendNEC(combinedCode);
-    Serial.println("Signal sent");
-    delay(2000);  // Wait before sending again
+void loop() {
+    // Add your code here
 }
-
-// void loop()
-// {
-//     Serial.println("Toggling power");
-// #if SEND_GLOBALCACHE
-//     irsend.sendGC(Samsung_power_toggle, 71);
-// #else   // SEND_GLOBALCACHE
-//     Serial.println("Can't send because SEND_GLOBALCACHE has been disabled.");
-// #endif  // SEND_GLOBALCACHE
-//     delay(10000);
-// }
