339a94963e0ecb9af4290ce0d3c3d2bf41c981c1
Snuffish <snuffish90@gmail.com>
2025-06-02 00:01:14 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,34 @@
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
+static const unsigned char PROGMEM image_music_radio_streaming_bits[] = {0x00,0x00,0x00,0x00,0x00,0x00,0x20,0x02,0x00,0x40,0x01,0x00,0x48,0x09,0x00,0x90,0x84,0x80,0xa5,0xd2,0x80,0xa9,0x4a,0x80,0xa5,0xd2,0x80,0x90,0x84,0x80,0x48,0x09,0x00,0x40,0x01,0x00,0x20,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+
 void setup() {
-    tft.initR(INITR_GREENTAB); // Initialize the display with the correct tab color
+    tft.initR(INITR_BLACKTAB); // Initialize the display with the correct tab color
     tft.fillScreen(ST7735_BLACK); // Clear the screen
     tft.setTextColor(ST7735_WHITE); // Set text color to white
     tft.setTextSize(2); // Set text size
     tft.setCursor(10, 10); // Set cursor position
     tft.print("Hello, World!"); // Print text
+
+    // tft.drawBitmap(2, 1, image_Layer_1_bits, 5, 4, 0xFFFF);*/
+
+    // // music_radio_streaming
+    // tft.drawBitmap(0, 0, image_music_radio_streaming_bits, 17, 16, 0xFFFF);
+    //
+    // // Layer 2
+    // tft.setTextColor(0xFFFF);
+    // tft.setTextSize(1);
+    // tft.setCursor(21, 5);
+    // tft.print("IR Signals");
+
+}
+
+void draw(void) {
+    // Layer 1
+
 }
 
 void loop() {
-    // Add your code here
+    draw();
 }
