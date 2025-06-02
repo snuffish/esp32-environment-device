0d5c8cdb95bb1c8492fb79b9f5953734c54cba24
Snuffish <snuffish90@gmail.com>
2025-06-02 21:56:51 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,112 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_ST7735.h>
+/*
+ * --------------------------------------------------------------------------------------------------------------------
+ * Example sketch/program showing how to read new NUID from a PICC to serial.
+ * --------------------------------------------------------------------------------------------------------------------
+ * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
+ *
+ * Example sketch/program showing how to the read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
+ * Reader on the Arduino SPI interface.
+ *
+ * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
+ * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
+ * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
+ * will show the type, and the NUID if a new card has been detected. Note: you may see "Timeout in communication" messages
+ * when removing the PICC from reading distance too early.
+ *
+ * @license Released into the public domain.
+ *
+ * Typical pin layout used:
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino  Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101      Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST         9             5         D9        RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI        11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO        12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52       D13        ICSP-3           15
+ *
+ * More pin layouts for other boards can be found here: https://github.com/miguelbalboa/rfid#pin-layout
+ */
+
 #include <SPI.h>
+#include <MFRC522.h>
 
-#define ST7735_DRIVER
-#define TFT_WIDTH 128
-#define TFT_HEIGHT 160
-#define ST7735_INITB
-#define TFT_CS 5 // Chip select control pin
-#define TFT_DC 2 // Data Command control pin
-#define TFT_RST 4 // Reset pin (optional)
-#define SPI_FREQUENCY 27000000
+#define SS_PIN 5
+#define RST_PIN 0
 
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
-static const unsigned char PROGMEM image_music_radio_broadcast_bits[] = {0x07,0xc0,0x18,0x30,0x27,0xc8,0x48,0x24,0x93,0x92,0xa4,0x4a,0xa9,0x2a,0xa3,0x8a,0x06,0xc0,0x03,0x80,0x01,0x00,0x03,0x80,0x02,0x80,0x06,0xc0,0x04,0x40};
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+
+/* Set your new UID here! */
+#define NEW_UID {0x5A, 0xAA, 0x01, 0x4F}
 
 void setup() {
-    tft.initR(INITR_BLACKTAB); // Initialize the display with the correct tab color
-    tft.fillScreen(ST7735_BLACK); // Clear the screen
-    tft.setTextColor(ST7735_WHITE); // Set text color to white
-    // tft.setTextSize(2); // Set text size
-    // tft.setCursor(10, 10); // Set cursor position
-    // tft.print("Hello, World!"); // Print text
-
-    // tft.drawBitmap(2, 1, image_Layer_1_bits, 5, 4, 0xFFFF);*/
-
-    // // music_radio_streaming
-    // tft.drawBitmap(0, 0, image_music_radio_streaming_bits, 17, 16, 0xFFFF);
-    //
-    // // Layer 2
-    // tft.setTextColor(0xFFFF);
-    // tft.setTextSize(1);
-    // tft.setCursor(21, 5);
-    // tft.print("IR Signals");
-
+  Serial.begin(9600);  // Initialize serial communications with the PC
+  while (!Serial);     // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();         // Init SPI bus
+  mfrc522.PCD_Init();  // Init MFRC522 card
+  Serial.println(F("Warning: this example overwrites the UID of your UID changeable card, use with care!"));
 }
 
-void draw() {
-    tft.drawBitmap(3, 2, image_music_radio_broadcast_bits, 15, 15, 0xFFFF);
+// Setting the UID can be as simple as this:
+//void loop() {
+//  byte newUid[] = NEW_UID;
+//  if ( mfrc522.MIFARE_SetUid(newUid, (byte)4, true) ) {
+//    Serial.println("Wrote new UID to card.");
+//  }
+//  delay(1000);
+//}
 
-    tft.setTextColor(0xFFFF);
-    tft.setTextSize(1);
-    tft.setCursor(24, 7);
-    tft.print("RFID");
-
-    tft.drawLine(0, 21, 128, 21, 0xFFFF);
-}
-
+// But of course this is a more proper approach
 void loop() {
-    draw();
+
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle. And if present, select one.
+  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
+    delay(50);
+    return;
+  }
+
+  // Now a card is selected. The UID and SAK is in mfrc522.uid.
+
+  // Dump UID
+  Serial.print(F("Card UID:"));
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+  Serial.println();
+
+  // Dump PICC type
+//  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+//  Serial.print(F("PICC type: "));
+//  Serial.print(mfrc522.PICC_GetTypeName(piccType));
+//  Serial.print(F(" (SAK "));
+//  Serial.print(mfrc522.uid.sak);
+//  Serial.print(")\r\n");
+//  if (  piccType != MFRC522::PICC_TYPE_MIFARE_MINI
+//    &&  piccType != MFRC522::PICC_TYPE_MIFARE_1K
+//    &&  piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+//    Serial.println(F("This sample only works with MIFARE Classic cards."));
+//    return;
+//  }
+
+  // Set new UID
+  byte newUid[] = NEW_UID;
+  if ( mfrc522.MIFARE_SetUid(newUid, (byte)4, true) ) {
+    Serial.println(F("Wrote new UID to card."));
+  }
+
+  // Halt PICC and re-select it so DumpToSerial doesn't get confused
+  mfrc522.PICC_HaltA();
+  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
+    return;
+  }
+
+  // Dump the new memory contents
+  Serial.println(F("New UID and contents:"));
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+
+  delay(2000);
 }
