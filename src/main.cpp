09e0fc5a3482c998cde4ccc9b90b18502816e4ec
Snuffish <snuffish90@gmail.com>
2025-06-12 00:49:11 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 #include <SPI.h>
 #include <vector>
 
+#include "ESP32Encoder.h"
 #include "icons.h"
+#include "menu.h"
+#include "OneButton.h"
 
 #define ST7735_DRIVER
 #define TFT_WIDTH 128
@@ -14,44 +17,75 @@
 #define TFT_RST 4 // Reset pin (optional)
 #define SPI_FREQUENCY 27000000
 
-auto tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+#define ENCODER_CLK 32
+#define ENCODER_DT 33
+#define ENCODER_BUTTON 25
 
-void drawMenu();
-
-void setup()
-{
-    tft.initR(INITR_BLACKTAB); // Initialize the display with the correct tab color
-    tft.fillScreen(ST7735_BLACK); // Clear the screen
-    tft.setTextColor(ST7735_WHITE); // Set text color to white
-}
-
-// Fix the typo: rifd_logo_bits should be rfid_logo_bits
-
-
-#define MENU_HEIGHT 17
-
-// Update the struct to use a pointer to unsigned char array
-typedef struct
-{
-    const unsigned char* icon_bits; // Changed to pointer
-    std::string name;
-} MenuItem;
-
-// Now the menu_items initialization will work
-std::vector<MenuItem> menu_items = {
+std::vector<MenuItem> default_menu = {
     {rfid_logo_bits, "RFID"},
     {wifi_logo_bits, "WiFi"},
     {ir_signal_logo_bits, "IR Signal"},
 };
 
+std::vector<MenuItem> test_menu = {
+    {rfid_logo_bits, "Test1"},
+    {rfid_logo_bits, "Test2"},
+    {rfid_logo_bits, "Test3"},
+};
+
+auto menu_items = default_menu;
+
+auto tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+ESP32Encoder encoder;
+OneButton button(ENCODER_BUTTON);
+
+std::string menuTitle = "MENU";
+
+void drawMenu();
+
+void enterClick();
+
+void longPress();
+
+void setup()
+{
+    Serial.begin(9600);
+
+    tft.initR(INITR_BLACKTAB); // Initialize the display with the correct tab color
+    tft.fillScreen(ST7735_BLACK); // Clear the screen
+    tft.setTextColor(ST7735_WHITE); // Set text color to white
+
+    drawMenu();
+
+    encoder.attachHalfQuad(ENCODER_CLK, ENCODER_DT);
+
+    button.attachClick(enterClick);
+    button.attachLongPressStart(longPress);
+}
+
 auto selected_menu_item = &menu_items[0];
 
 void item(const MenuItem& menu_item);
 
-#define MENU_ITEM_HEIGHT 25
-#define MENU_ITEM_PADDING 7
-
 int current_menu_item = 0;
+
+void enterClick()
+{
+    Serial.println("ENTER CLICK");
+
+    menuTitle = selected_menu_item->name;
+    menu_items = test_menu;
+
+    drawMenu();
+};
+
+void longPress()
+{
+    menuTitle = "MENU";
+    menu_items = default_menu;
+
+    drawMenu();
+};
 
 void drawItem(const MenuItem& item)
 {
@@ -79,7 +113,7 @@ void drawMenu()
 
     tft.drawRect(0, 0, 128, MENU_HEIGHT, 0xFFFF);
     tft.setCursor(6, 7);
-    tft.print("MENU");
+    tft.print(menuTitle.c_str());
 
     tft.drawBitmap(112, 1, battery_indicator_icon_bits, 16, 16, 0x4D6A);
 
@@ -89,9 +123,57 @@ void drawMenu()
     }
 }
 
-
-void loop(void)
+void moveNext()
 {
+    Serial.println("MOVE NEXT");
+
+    // Calculate the index of the current item
+    size_t current_index = selected_menu_item - &menu_items[0];
+
+    // Move to next item or wrap around
+    current_index = (current_index + 1) % menu_items.size();
+    selected_menu_item = &menu_items[current_index];
+
     drawMenu();
-    delay(1000);
+}
+
+void movePrevious()
+{
+    Serial.println("MOVE PREVIOUS");
+
+    // Calculate the index of the current item
+    size_t current_index = selected_menu_item - &menu_items[0];
+
+    // Move to previous item or wrap around
+    if (current_index == 0) {
+        current_index = menu_items.size() - 1; // Wrap to end
+    } else {
+        current_index--;
+    }
+    selected_menu_item = &menu_items[current_index];
+
+    drawMenu();
+}
+
+auto last_position = 0;
+
+void loop()
+{
+    button.tick();
+
+    long newPosition = encoder.getCount() / 2;
+    Serial.println(newPosition);
+
+    if (newPosition > last_position)
+    {
+        moveNext();
+    }
+    else if (newPosition < last_position)
+    {
+        movePrevious();
+    }
+
+    last_position = newPosition;
+
+    delay(10);
 }
